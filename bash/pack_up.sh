404758f1d44960b589f594c084f0ba922dfa5b7b
sn0wfree <snowfreedom0815@gmail.com>
2019-10-23 12:45:19 +0800

    Update pack_up.sh

--- bash/pack_up.sh
+++ bash/pack_up.sh
@@ -6,3 +6,36 @@ function pack_project(){
   pip download -d ./Dependency -r requirements.txt
 
 }
+
+function install_package(){
+Path=./Dependency
+Pip install --no-index --find-links=${path} -r requirements.txt
+
+}
+
+function unzip_dependency(){
+unzip  Dependency.zip
+
+}
+
+function zip_dependency(){
+pipreqs --use-local . --force
+zip -r Dependency.zip Dependency
+zip -m Dependency.zip requirements.txt
+ 
+
+}
+
+echo $1
+if [[ $1 -eq 0 ]]; then
+echo ‘package mode enable’
+pack_project
+zip_dependency
+
+fi 
+
+if [[ $1 -eq 1 ]]; then
+echo ‘install mode enable’
+unzip_dependency
+install_package
+fi 
